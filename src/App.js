ae255a4dec312d6e26cddf505d3c7d0648d013ea
Edson Tégila <etegila@gmail.com>
2021-04-20 20:35:30 -0300

    material.ui + react web js
    - simple page application without routing

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+let counter = 1;
+function createData(exchange, price, amount) {
+  return {
+    key: counter++,
+    exchange,
+    price: Math.round((price + Number.EPSILON) * 100) / 100,
+    amount,
+  };
+}
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 200,
+  },
+});
+
+export default function App() {
+  const classes = useStyles();
+  const [rows, setRows] = useState([]);
+  const webSocket = useRef(null);
+
+  useEffect(() => {
+    // ['bitfinex', 23320, 0.00012313]
+    webSocket.current = new WebSocket("wss://c.tegila.com.br/wss");
+    webSocket.current.onmessage = (row) => {
+      // console.log(row);
+      const tick = JSON.parse(row.data);
+      // console.log(tick);
+      setRows((prev) => [createData(...tick), ...prev]);
+    };
+    return () => webSocket.current.close();
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Exchange</StyledTableCell>
+            <StyledTableCell align="right">Prize</StyledTableCell>
+            <StyledTableCell align="right">Amount</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.key}>
+              <StyledTableCell align="right">{row.exchange}</StyledTableCell>
+              <StyledTableCell align="right">{row.price}</StyledTableCell>
+              <StyledTableCell align="right">{row.amount}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import injectTapEventPlugin from 'react-tap-event-plugin';   // add
+
+import './index.css';
+import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// injectTapEventPlugin();  // add
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
